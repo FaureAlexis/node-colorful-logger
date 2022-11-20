913ba7cca3880c473aa2dd23fbb9245799ffded3
Alexis Faure <alexis.faure@epitech.eu>
2022-11-20 10:07:13 +0100

    [TS-LOGGER] - Add types declaration

---
+++ types/logger.ts
@@ -0,0 +1,33 @@
+export enum Services {
+  AUTH = 'API Authentification',
+  USER = 'User Authentification',
+  LOG = 'Logger',
+  DATABASE = 'Database',
+  MAIL = 'Mail service',
+  MIDDLEWARE = 'Errors middleware',
+  API = 'API',
+}
+
+export enum LogType {
+  INFO = 'INFO',
+  ERROR = 'ERROR',
+  WARN = 'WARN',
+  DEBUG = 'DEBUG',
+  SUCCESS = 'SUCCESS',
+}
+
+export enum LogColor {
+  INFO = '\x1b[34m',
+  ERROR = '\x1b[31m',
+  WARN = '\x1b[33m',
+  DEBUG = '\x1b[36m',
+  SUCCESS = '\x1b[32m',
+  END = '\x1b[0m',
+}
+
+export interface Log {
+  type: LogType
+  message: String | undefined;
+  color: LogColor
+  service?: Services
+}
